a23dcf257765c428287efc0c4e549974d3713f41
toriningenn <annamihailova72@yandex.com>
2021-05-24 10:34:46 +0200

    Pagination and simple UI added.

--- src/components/WeatherTable.tsx
+++ src/components/WeatherTable.tsx
@@ -82,7 +82,8 @@ const WeatherTable = (props: {}) => {
             data,
             initialState: {pageIndex: 2},
         },
-        usePagination);
+        usePagination
+    );
 
 
     const {
@@ -101,8 +102,25 @@ const WeatherTable = (props: {}) => {
         setPageSize,
         state: {pageIndex, pageSize},
     } = tableInstance;
-    
-    return (<table {...getTableProps}>
+
+    return (
+        <>
+      <pre>
+        <code>
+          {JSON.stringify(
+              {
+                  pageIndex,
+                  pageSize,
+                  pageCount,
+                  canNextPage,
+                  canPreviousPage,
+              },
+              null,
+              2
+          )}
+        </code>
+      </pre>
+        <table {...getTableProps}>
         <thead>
         {headerGroups.map(headerGroup => <tr {...headerGroup.getHeaderGroupProps()}>
             {headerGroup.headers.map(column => <th{...column.getHeaderProps}>{column.render('Header')}</th>)}
@@ -115,7 +133,43 @@ const WeatherTable = (props: {}) => {
                 <td{...cell.getCellProps}>{cell.render('Cell')}</td>)}</tr>
         })}
         </tbody>
-    </table>)
+    </table>
+
+            <div className="pagination">
+                <button onClick={()=>gotoPage(0)} disabled={!canPreviousPage}>
+                    {'<<'}
+                </button>
+                <button onClick={()=>previousPage()} disabled={!canPreviousPage}>
+                    {'<'}
+                </button>
+                <button onClick={()=> nextPage()} disabled={!canNextPage}>
+                    {'>'}
+                </button>
+                <button onClick={()=>gotoPage(pageCount-1)} disabled={!canNextPage}>
+                    {'>>'}
+                </button>
+                <span>
+                    Page <strong>{pageIndex + 1} of {pageOptions.length}</strong>
+                </span>
+                <span>
+           | Go to page:{' '}
+           <input type="number"
+           defaultValue={pageIndex + 1}
+           onChange={e=>{const page = e.target.value ? Number(e.target.value) - 1 : 0;
+           gotoPage(page)}}
+           style={{ width: '100px' }}/>
+                    </span>
+                <select value={pageSize}
+                 onChange={e=>{setPageSize(Number(e.target.value))}}>
+                    {[10,50,100].map(pageSize => (
+                        <option key={pageSize} value={pageSize}>
+                            Show {pageSize}
+                        </option>
+                    ))}
+                    </select>
+            </div>
+            </>
+    )
 }
 
 export default WeatherTable;
